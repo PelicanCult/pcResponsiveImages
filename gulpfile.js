ccd882087c7091404446c72fa45095a49557bfa1
Kurt Swickerath <kswick1@gmail.com>
2015-02-28 21:48:03 -0500

    updated gulp and directories

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 var gulp = require('gulp'),
 	sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
 
 // define tasks here
-gulp.task('uglify', function() {
-  gulp.src('*.js')
-    .pipe(uglify('pelicanCult_responsive_1.0.min.js', {
-      outSourceMap: true
-    }))
-});
-
 gulp.task('compress', function() {
-  gulp.src('*.js')
-    .pipe(uglify('pelicanCult_responsive_1.0.min.js'))
-    .pipe(gulp.dest('dist'))
+  gulp.src('pelicanCult_responsive_1.0.js')
+    .pipe(uglify({outSourceMap: true}))    
+    .pipe(rename(function (path) {
+        if(path.extname === '.js') {
+            path.basename += '.min';
+        }
+    }))
+    .pipe(gulp.dest('./'))
 });
